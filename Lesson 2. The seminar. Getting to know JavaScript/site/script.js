5f4fe063199e5883e4c1003ca8ab3b68dfce34d7
yuriy_pidenko <yuriy.pidenko@gmail.com>
2024-11-05 21:05:29 +0300

    Lesson 2 Seminar.

---
+++ Lesson 2. The seminar. Getting to know JavaScript/site/script.js
@@ -0,0 +1,52 @@
+// Задание 2
+const num = '123';
+let a = 5;
+const b = 1 + 2 + 3;
+
+a = 7;
+
+console.log(a);
+console.log(b);
+
+// Коментарий
+/*Коментарий
+много
+строчный */
+
+// Задание 3 
+const c = 10;
+const d = 2
+
+let amounts = c + d;
+let differences = c - d;
+let insights = c * d;
+let private = c / d;
+
+console.log(`
+   Cумма ${amounts} 
+   Разность ${differences}
+   Прозведения ${insights}
+   Частное ${private}
+   `);
+console.log(amounts + differences + insights + private);
+
+// Задание 5
+const nuberA = 1.5;
+const nuberB = 0.75;
+
+console.log(nuberA + nuberB);
+
+const nuberD = -5;
+console.log(-nuberD);
+
+// Задание 6 
+const userName = 'Юра'
+const userLastName = 'Петров'
+const java = 'Java'
+const script = 'Script'
+const hello = 'hello'
+const world = 'world'
+
+console.log(`${userName}  ${userLastName}`);
+console.log(java + script);
+console.log(hello, ' ' + world);

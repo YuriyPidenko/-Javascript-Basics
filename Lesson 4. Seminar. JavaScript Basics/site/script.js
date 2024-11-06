0ce563175a24c2f4089b1e98cd8a715258de730b
yuriy_pidenko <yuriy.pidenko@gmail.com>
2024-11-06 20:55:27 +0300

    lesson 4

---
+++ Lesson 4. Seminar. JavaScript Basics/site/script.js
@@ -0,0 +1,58 @@
+// Задание 1
+
+// let userAge = prompt('Сколько вам лет?')
+// alert(`Вам возраст ${userAge}`)
+
+// let userName = prompt('Как вас зовут ?')
+// alert(`Добро пожаловать на сайт ${userName}`)
+
+
+//Задание 2
+
+// let a = 13
+// let b = 5
+
+// console.log(a % b);
+
+// let q;
+// alert(q);
+
+// alert('abc' * 3);
+
+// alert(1 / 0);
+// alert(-1 / 0);
+// alert('2' * '3');
+
+
+// Задание 3
+// let a = '2'
+// let b = '3'
+// console.log(a + b);
+
+
+// let number = Number(prompt(' Введите первое число'))
+// let number2 = Number(prompt(' Введите второе число'))
+// alert(`Сумма чисел равна ${number + number2}`)
+// alert(`Разность чисел равна ${number - number2}`)
+// alert(`Произведение чисел равно ${number * number2}`)
+// alert(`Частное чисел равно ${number / number2}`)
+// alert(` Остаток от деления чисел равен ${number % number2}`)
+
+
+//Задание 5
+// let number = Number(prompt(' Введите число'))
+// let number2 = Number(prompt(' Введите второе число'))
+
+// if (number > number2) {
+//     console.log(`Число ${number} больше ${number2}`);
+// } else if (number < number2) {
+//     console.log(`Число ${number} меньше ${number2}`);
+// } else if (number === number) {
+//     console.log(`Число ${number} равно  ${number2}`);
+// } else {
+//     console.log('вы вели не число');
+// }
+
+
+let num = Number(prompt(' Введите первое число'))
+console.log((num < 15 && num > 0) ? 'Ура' : 'попробуйте в следущи раз');
